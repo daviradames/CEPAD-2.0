80363b5591712ea8f2785918efb8194d1d8af946
Davi Radames <daviradames@gmail.com>
2020-06-20 14:00:32 -0300

    Versão Inicial – SistemaRecados

---
+++ Prototótipo atualizado 2.0/Exer2/Content/Script/form_Animal.js
@@ -0,0 +1,9 @@
+﻿
+    function mascaraData(campoData){
+    var data = campoData.value;
+    if (data.length == 2){
+        data = data + '/';
+    document.forms[0].data.value = data;
+    return true;
+    }
+    }
---
+++ Prototótipo atualizado 2.0/Exer2/Controllers/AdminController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Exer2.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult LoginADM()
+        {
+            return View();
+        }
+        public ActionResult cadastroADM()
+        {
+            return View();
+        }
+        public ActionResult AgendaADM()
+        {
+            return View();
+        }
+        public ActionResult AgendamentoADM()
+        {
+            return View();
+        }
+
+    }
+}
