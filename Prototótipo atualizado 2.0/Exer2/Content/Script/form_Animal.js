41833abb0b56e142ca23ad43d669f102179d26cf
Davi Radames <daviradames@gmail.com>
2020-06-23 13:46:44 -0300

    erro do banco

--- Prototótipo atualizado 2.0/Exer2/Content/Script/form_Animal.js
+++
@@ -1,9 +0,0 @@
-﻿
-    function mascaraData(campoData){
-    var data = campoData.value;
-    if (data.length == 2){
-        data = data + '/';
-    document.forms[0].data.value = data;
-    return true;
-    }
-    }
